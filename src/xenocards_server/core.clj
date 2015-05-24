265bfd94956087b22b7894fa9756637da3d3985f
Anton Volokitin <antender@live.ru>
2015-05-24 19:44:59 +0300

    major refactoring

--- src/xenocards_server/core.clj
+++ src/xenocards_server/core.clj
@@ -1,18 +1,18 @@
 (ns xenocards-server.core
-  (:use org.httpkit.server)
+  (:require [org.httpkit.server :refer :all]
+            [xenocards-server.game :as game])
   (:gen-class))
 
 (declare exit-room)
 
-(def handlers (atom {}))
-
 (def registered-names (atom {}))
-
 (def users (atom {}))
-
 (def rooms (atom {}))
+(def channels (atom {}))
+(def nextroomid (atom 0))
 
-(def userlocations (atom {}))
+(defn string-to-int [string]
+  (Integer/parseInt (clojure.string/replace string #"[^0-9]" "")))
 
 (defmacro defcommands [name channel commanddata & body]
   `(defn ~name [~channel ~commanddata]
@@ -21,7 +21,7 @@
         (condp = (~'command 0)
          ~@(mapcat (fn [commandhandler#]
             `(~(nth commandhandler# 0)
-               (if (and (not= ~(nth commandhandler# 1) 0) (not= (count ~'command) ~(nth commandhandler# 1)))
+               (if ~(if (= (nth commandhandler# 1) 0) 'true `(not= (count ~'command) ~(nth commandhandler# 1)))
                    (send! ~channel "invalid number of arguments")
                   ~(nth commandhandler# 2))))
                      (partition 3 body))
@@ -31,51 +31,78 @@
   "exit" 1
     (do
       (exit-room channel)
-      (send! channel "exited room")
-      (swap! userlocations dissoc channel))
+      (send! channel "exited room"))
+  "roomid" 1
+    (send! channel (str ((@users channel) :room)))
   "roomname" 1
-    (send! channel (@userlocations channel))
+    (send! channel ((@rooms ((@users channel) :room)) :name))
   "myrole" 1
-    (send! channel (if (= ((@rooms (@userlocations channel)) :host) (@users channel)) "host" "opponent"))
+    (send! channel (if (= ((@rooms ((@users channel) :room)) :host) channel) "host" "opponent"))
+  "ready" 1
+    ;TODO ready
+    (send! channel "ready")
   "opponentsname" 1
-    (let [role (if (= ((@rooms (@userlocations channel)) :host) (@users channel)) :host :opponent)]
-        (if-let [name ((@rooms (@userlocations channel)) (if (= role :host) :opponent :host))]
-          (send! channel name)
-          (send! channel "no opponent"))))
+    (let [room (@rooms ((@users channel) :room))]
+      (if (= (room :host) channel)
+        (if (contains? room :opponent)
+          (send! channel ((@users (room :opponent)) :name))
+          (send! channel "no opponent"))
+        (send! channel ((@users (room :host)) :name)))))
 
 (defcommands receive-lobby channel commanddata
   "listplayers" 1
-    (send! channel (clojure.string/join " " (vals @users)))
+    (send! channel (clojure.string/join " " (map :name (vals @users))))
   "listrooms" 1
-    (send! channel (clojure.string/join " " (keys @rooms)))
+    (send! channel (clojure.string/join "\n"
+      (map
+        (fn [key]
+          (str key " " ((@rooms key) :name)))
+        (keys @rooms))))
   "myname" 1
-    (send! channel (@users channel))
+    (send! channel ((@users channel) :name))
   "createroom" 2
-    (if (@rooms (command 1))
+    (if (contains? @rooms (command 1))
       (send! channel (str "room " (command 1) "already exists"))
-      (do
-        (swap! rooms assoc (command 1) {:host (@users channel)})
-        (swap! userlocations assoc channel (command 1))
-        (swap! handlers assoc channel receive-room)
+      (let [roomid (swap! nextroomid inc)]
+        (swap! rooms assoc roomid
+          {:host channel
+           :name (command 1)})
+        (swap! users assoc channel
+          (assoc (@users channel)
+            :room roomid
+            :handler receive-room))
         (send! channel (str "room " (command 1) " created"))))
   "joinroom" 2
-    (if-let [room (@rooms (command 1))]
-      (if (contains? room :opponent)
-        (send! channel "occupied")
-        (do
-          (swap! rooms assoc (command 1) {:opponent (@users channel)})
-          (swap! userlocations assoc channel (command 1))
-          (swap! handlers assoc channel receive-room)
-          (send! channel (str "entered room " (command 1)))))
-      (send! channel "no room with this name")))
+    (let [roomid (string-to-int (command 1))]
+      (if-let [room (@rooms roomid)]
+        (if (contains? room :opponent)
+          (send! channel "occupied")
+          (do
+            (swap! rooms assoc roomid
+              (assoc (@rooms roomid)
+                :opponent channel))
+            (swap! users assoc channel
+              (assoc (@users channel)
+                :room roomid
+                :handler receive-room))
+            (send! channel (str "entered room " ((@rooms roomid) :name)))))
+        (send! channel "no room with this name"))))
 
 (defcommands receive-signin channel commanddata
-  "connect" 3
+  "authorize" 3
     (if-let [password (@registered-names (command 1))]
       (if (= password (command 2))
         (do
-          (swap! users assoc channel (command 1))
-          (swap! handlers assoc channel receive-lobby)
+          (if (contains? @channels (command 1))
+            (let [previous-channel (@channels (command 1))]
+              (swap! users assoc channel
+                (@users previous-channel))
+              (swap! users dissoc previous-channel))
+              ;TODO resume game when reconnecting
+            (swap! users assoc channel
+              {:name (command 1)
+               :handler receive-lobby}))
+          (swap! channels assoc (command 1) channel)
           (send! channel "connected"))
         (send! channel "invalid password"))
       (send! channel "not found"))
@@ -89,27 +116,38 @@
 
 
 (defn exit-room [channel]
-  (if-let [roomname (@userlocations channel)]
-    (let [role (if (= ((@rooms roomname) :host (@users channel))) :host :opponent)]
-      (swap! rooms assoc roomname (dissoc (@rooms roomname) role))
-      (if (not (or (contains? (@rooms roomname) :host) (contains? (@rooms roomname) :opponent)))
-        (swap! rooms dissoc roomname))))
-      (swap! handlers assoc channel receive-lobby))
+  (if-let [roomid ((@users channel) :room)]
+    (let [role (if (= ((@rooms roomid) :host) channel) :host :opponent)]
+      (if (= role :opponent)
+        (swap! rooms assoc roomid (dissoc (@rooms roomid) :opponent))
+        (if-let [opponent ((@rooms roomid) :opponent)]
+          (do
+            (swap! rooms assoc roomid
+              (dissoc
+                (assoc
+                  (@rooms roomid)
+                  :host opponent)
+                :opponent))
+            (send! opponent "you are host now"))
+          (swap! rooms dissoc roomid)))))
+  (swap! users assoc channel
+    (dissoc
+      (assoc (@users channel)
+        :handler receive-lobby)
+      :room)))
 
 
 
-(defn handler [request]
+(defn server [request]
   (with-channel request channel
-    (swap! handlers assoc channel receive-signin)
+    (swap! users assoc channel {:handler receive-signin})
     (send! channel "welcome")
     (on-close channel
       (fn [status]
         (exit-room channel)
-        ;TODO pause current game on disconnect
-        (swap! handlers dissoc channel)
-        (swap! users dissoc channel)
-        (swap! userlocations dissoc channel)))
-    (on-receive channel (fn [data] ((get @handlers channel) channel data)))))
+        ;TODO pause current game on disconnect instead of disconnecting
+        (swap! users dissoc channel)))
+    (on-receive channel (fn [data] (((@users channel) :handler) channel data)))))
 
 (defn -main [& args]
-  (run-server handler {:port 9090}))
+  (run-server server {:port 9090}))
---
+++ src/xenocards_server/game.clj
@@ -0,0 +1,1 @@
+(ns xenocards-server.game)
